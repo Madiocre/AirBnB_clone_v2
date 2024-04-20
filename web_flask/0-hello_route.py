2d3e38627eaa803600e6e69d5b9a3d1309460741
madiocre <wolvirex@gmail.com>
2024-04-19 20:04:51 -0700

    flask testfaulty

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,8 +4,10 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-@app.route("/")
+@app.route('/', strict_slashes=False)
 def hello():
-    return "Hello, World!"
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/__init__.py
+++ web_flask/__init__.py
@@ -1,4 +1,1 @@
-#!/usr/bin/python3
-""" this is the init file """
-from flask import Flask
-app = Flask(__name__)
+
