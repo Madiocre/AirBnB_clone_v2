0041ba2a941179a0bce36743ecabc4d2f1543126
madiocre <wolvirex@gmail.com>
2024-04-21 02:32:52 +0200

    webflask 7

--- models/base_model.py
+++ models/base_model.py
@@ -4,9 +4,7 @@ import uuid
 import models
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
